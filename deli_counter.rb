edcd86c930d7c789e02fa509375813bf9d4d15e1
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-06-27 01:18:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def take_a_number(array, name)
   array.push(name)
   position = array.index(name)
- in line."
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
   return name, position 
 end 
 
