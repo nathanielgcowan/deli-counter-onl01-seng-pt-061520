0478f76bacdc8769571980214a65fa551028f71d
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-06-27 01:16:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ katz_deli = []
 def take_a_number
   array.push(name)
   positoin = array.index(name)
-  puts "Welcome, #{name}". You are number #{
+  puts "Welcome, #{name}". You are number #{array.index(name)+1}
 end
 
 def now_serving
